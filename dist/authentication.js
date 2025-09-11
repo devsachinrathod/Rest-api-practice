ec9e92d881d82597315bcdb44deeb8f8676806c6
Sachin Rathod <sr5631080@gmail.com>
2025-09-11 21:11:33 +0530

    all about the authentication

--- dist/authentication.js
+++ dist/authentication.js
@@ -5,34 +5,60 @@ const userSchema = zod.object({
     username: zod.string().email(),
     password: zod.string().min(6),
 });
-function GenerateToken(username, password) {
-    const result = userSchema.safeParse({ username, password });
-    if (!result.success) {
-        return null;
+function GenerateTokens(username, password) {
+    const results = userSchema.safeParse({ username, password });
+    if (!results.success) {
+        return " user is provides wrong inputs";
     }
-    const token = jwt.sign({ username }, jwtPassword);
+    const token = jwt.sign({ username, password }, jwtPassword);
     return token;
 }
-function decodeToken(token) {
-    try {
-        const decoded = jwt.decode(token);
-        return !!decoded;
+function forDecodingToken(token) {
+    const deCodedToken = jwt.decode(token);
+    return deCodedToken;
+}
+function verifyToken(token) {
+    if (!token) {
+        return "token is not provide";
     }
-    catch {
+    const verifyTokenVar = jwt.verify(token, jwtPassword);
+    if (!verifyToken) {
         return false;
     }
-}
-function VerifyToken(token) {
-    try {
-        jwt.verify(token, jwtPassword);
+    else {
         return true;
     }
-    catch {
-        return false;
-    }
 }
-const token = GenerateToken("akhil@example.com", "mypassword");
-console.log("Generated Token:", token);
-console.log("Can decode token:", decodeToken(token));
-console.log("Can verify token:", VerifyToken(token));
+console.log(GenerateTokens("sr5631080@gmail.com", "test12"));
+console.log(forDecodingToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InNyNTYzMTA4MEBnbWFpbC5jb20iLCJwYXNzd29yZCI6InRlc3QxMiIsImlhdCI6MTc1NzYwNDcxMX0.wxilwMkMtHSyIOdiqJIEho3Ec_icniR2cOr-I2XNcuc"));
+console.log(verifyToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InNyNTYzMTA4MEBnbWFpbC5jb20iLCJwYXNzd29yZCI6InRlc3QxMiIsImlhdCI6MTc1NzYwNDcxMX0.wxilwMkMtHSyIOdiqJIEho3Ec_icniR2cOr-I2XNcuc"));
+// function GenerateToken(username:string, password:string) {
+// const result = userSchema.safeParse({ username, password });
+// console.log(result);
+// if (!result.success) {
+// return null;
+// }
+// const token = jwt.sign({ username }, jwtPassword);
+// return token;
+// }
+// function decodeToken(token:any) {
+// try {
+// const decoded = jwt.decode(token);
+// return !!decoded;
+// } catch {
+// return false;
+// }
+// }
+// function VerifyToken(token:any) {
+// try {
+// jwt.verify(token, jwtPassword);
+// return true;
+// } catch {
+// return false;
+// }
+// }
+// const token = GenerateToken("akhil@example.com", "mypassword");
+// console.log("Generated Token:", token);
+// console.log("Can decode token:", decodeToken(token));
+// console.log("Can verify token:", VerifyToken(token));
 //# sourceMappingURL=authentication.js.map
--- src/authentication.ts
+++ src/authentication.ts
@@ -8,40 +8,85 @@ username: zod.string().email(),
 password: zod.string().min(6),
 });
 
+function GenerateTokens(username:string , password:string){
+    const results = userSchema.safeParse({username , password});
+    
+    if(!results.success){
+        return " user is provides wrong inputs";
+    }
+    const token = jwt.sign({username , password}, jwtPassword);
 
-function GenerateToken(username:string, password:string) {
-const result = userSchema.safeParse({ username, password });
+    return token;
 
-if (!result.success) {
-return null;
+}
+function forDecodingToken(token:any){
+  const deCodedToken = jwt.decode(token);
+
+  return deCodedToken;
 }
 
-const token = jwt.sign({ username }, jwtPassword);
-return token;
+function verifyToken(token:any){
+    if(!token){
+        return "token is not provide";
+    }
+    const verifyTokenVar = jwt.verify(token , jwtPassword);
+    if(!verifyToken){
+       return false;
+    }
+    else{
+        return true;
+    }
 }
 
-
-function decodeToken(token:any) {
-try {
-const decoded = jwt.decode(token);
-return !!decoded;
-} catch {
-return false;
-}
-}
+console.log(GenerateTokens("sr5631080@gmail.com", "test12"));
+console.log(forDecodingToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InNyNTYzMTA4MEBnbWFpbC5jb20iLCJwYXNzd29yZCI6InRlc3QxMiIsImlhdCI6MTc1NzYwNDcxMX0.wxilwMkMtHSyIOdiqJIEho3Ec_icniR2cOr-I2XNcuc"));
+console.log(verifyToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InNyNTYzMTA4MEBnbWFpbC5jb20iLCJwYXNzd29yZCI6InRlc3QxMiIsImlhdCI6MTc1NzYwNDcxMX0.wxilwMkMtHSyIOdiqJIEho3Ec_icniR2cOr-I2XNcuc"));
 
 
-function VerifyToken(token:any) {
-try {
-jwt.verify(token, jwtPassword);
-return true;
-} catch {
-return false;
-}
-}
 
-const token = GenerateToken("akhil@example.com", "mypassword");
-console.log("Generated Token:", token);
 
-console.log("Can decode token:", decodeToken(token));
-console.log("Can verify token:", VerifyToken(token));
+
+
+
+
+
+
+
+
+
+// function GenerateToken(username:string, password:string) {
+// const result = userSchema.safeParse({ username, password });
+// console.log(result);
+// if (!result.success) {
+// return null;
+// }
+
+// const token = jwt.sign({ username }, jwtPassword);
+// return token;
+// }
+
+
+// function decodeToken(token:any) {
+// try {
+// const decoded = jwt.decode(token);
+// return !!decoded;
+// } catch {
+// return false;
+// }
+// }
+
+
+// function VerifyToken(token:any) {
+// try {
+// jwt.verify(token, jwtPassword);
+// return true;
+// } catch {
+// return false;
+// }
+// }
+
+// const token = GenerateToken("akhil@example.com", "mypassword");
+// console.log("Generated Token:", token);
+
+// console.log("Can decode token:", decodeToken(token));
+// console.log("Can verify token:", VerifyToken(token));
